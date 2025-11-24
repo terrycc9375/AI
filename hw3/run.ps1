b192ad7ad25e8e2d13d7f82676722bfb081de27d
terrycc9375 <terrycc.ee12@nycu.edu.tw>
2025-11-24 20:03:53 +0800

    train result of bertweet

--- hw3/run.ps1
+++ hw3/run.ps1
@@ -1,1 +1,1 @@
-python nlp.py --epochs 16 --batch_size 8 --seed 42 --model_name "FacebookAI/roberta-large" --head "large"
+python nlp.py --epochs 10 --batch_size 32 --seed 42 --model_name "vinai/bertweet-base" --head "large"
