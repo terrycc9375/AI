fcfc98fe8ff6ae169c57f633753c4d10cd6cf53f
terrycc9375 <terrycc.ee12@nycu.edu.tw>
2025-12-04 23:39:22 +0800

    small changes to rich porgress

--- hw4/yolov8.py
+++ hw4/yolov8.py
@@ -1,11 +1,34 @@
 from ultralytics.models.yolo.model import YOLO
+from ultralytics.engine.trainer import BaseTrainer
 import rich, rich.progress
+import time
 
 class RichProgressCallback:
     def __init__(self):
         self.progress = rich.progress.Progress(
 			rich.progress.TextColumn("[bold blue] Epoch {task.fields[epochs]}/{task.fields[total_epochs]}"),
+            rich.progress.BarColumn(bar_width=20),
+            rich.progress.MofNCompleteColumn(),
+            rich.progress.TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
+            rich.progress.TextColumn("•"),
+            rich.progress.TextColumn("{task.fields[time_info]}"),
+            transient=False
 		)
+        
+        self.task_id = None
+        self.start_time = None
+        
+    def on_train_start(self, trainer: BaseTrainer):
+        self.task_id = self.progress.add_task(
+            description=f"[green]Training",
+            epoch=0,
+            total=trainer.epochs
+        )
+        self.progress.start()
+
+    def on_train_end(self, trainer: BaseTrainer):
+        self.progress.update(self.task_id, completed=trainer.epochs)
+        self.progress.stop()
 
 def train():
     model = YOLO("yolo8n.pt")
